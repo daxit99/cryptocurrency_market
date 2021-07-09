c6a44220a3144f52630483df9f0d3eee5ab727a4
daxit99 <73168093+daxit99@users.noreply.github.com>
2021-07-10 00:20:00 +0500

    Update Coin.js

--- src/Coin.js
+++ src/Coin.js
@@ -11,15 +11,15 @@ const coin = ({name,image,Symbol,price,volume,priceChange,marketcap }) => {
                     <p className="coin-symbol">{Symbol}</p>
                 </div>
                 <div className="coin-data">
-                    <p className="coin-price">₹{price}</p>
-                    <p className="coin-volume">₹{volume.toLocaleString()}</p>
+                    <p className="coin-price">${price}</p>
+                    <p className="coin-volume">${volume.toLocaleString()}</p>
                     {priceChange < 0 ? (
                         <p className="coin-percent red">{priceChange.toFixed(2)}% ↧</p>
                     ) : (
                         <p className="coin-percent green">{priceChange.toFixed(2)}% ↥</p>
                     )}
 
-                    <p className="coin-marketcap">MKT Cap:₹{marketcap.toLocaleString()}</p>
+                    <p className="coin-marketcap">MKT Cap:${marketcap.toLocaleString()}</p>
                 </div>
             </div>
 
